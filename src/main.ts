497d1c0559c69d4b1e8dce475e9736562d6ae963
kthss01 <kthss01@naver.com>
2026-02-19 15:15:39 +0900

    Add pooled EnvironmentManager and hook into render loop

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createSceneBundle } from './core/scene';
 import { createGroundPool } from './world/groundPool';
 import { createRenderLoop } from './core/loop';
 import { setupDebugControls } from './input/debugToggle';
+import { EnvironmentManager } from './world/environment-manager.js';
+import { appConfig } from './config';
 
 const container = document.querySelector<HTMLElement>('#app');
 if (!container) throw new Error('Missing #app container');
@@ -11,12 +13,17 @@ const { scene, camera, renderer, onResize } = createSceneBundle(container);
 const groundPool = createGroundPool();
 scene.add(groundPool.group);
 
+const envManager = new EnvironmentManager(scene, undefined, appConfig.environment);
+envManager.init();
+let cameraProgress = 0;
+
 const debug = setupDebugControls(camera, renderer.domElement, scene);
 
 window.addEventListener('resize', onResize);
 
 const loop = createRenderLoop(renderer, scene, camera, (delta) => {
   groundPool.update(delta);
+  cameraProgress = envManager.update(cameraProgress, delta, appConfig.ground.speed);
   debug.update();
 });
 
